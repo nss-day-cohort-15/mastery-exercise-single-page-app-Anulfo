59e52d948afd1bb62d9bfc645e9db68bba618af4
Anulfo <noaharkling@gmail.com>
2016-08-03 12:13:35 -0500

    now it does replace the description, not permanently though

--- eventhand.js
+++ eventhand.js
@@ -1,18 +1,33 @@
+
 var globalGo = function() {
     var listenArea = document.getElementsByClassName("modifiable");
         console.log(listenArea.length);
         for(var i = 0; i < listenArea.length; i++) {
-                listenArea.item(i).addEventListener ("click", function () {
+                listenArea.item(i).addEventListener ("click", 
+                function (){
+                    console.log("Clicked");
                     handleModifiableCLick();
-                });
-            }
-}
-
-function handleModifiableCLick(mouseEvent) {
-    var elementClicked = event.target.innerHTML;
-    console.log ("Element Clicked on", elementClicked);
-    document.getElementById("carSpecToChange").focus();
+                })
+        }
     
-
 }
+
+function handleModifiableCLick() {
+    console.log ("Element Clicked on", event.target);
+    document.getElementById("inputField").focus();
+    // return {
+    //     elementClicked;
+    // }
+    mirrorText(event.target);
+}
+
+
+function mirrorText (elementClicked) {
+    var inputField = document.getElementById("inputField");
+    inputField.addEventListener("keyup", function (evt) {
+        elementClicked.innerHTML = inputField.value;
+    });
+}
+
+
 
