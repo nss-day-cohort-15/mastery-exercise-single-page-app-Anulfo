0b84a670d0d03c764f0c483e69078b74fdff8bb0
Anulfo <noaharkling@gmail.com>
2016-08-03 14:20:50 -0500

    now to the button thing

--- displayerfunct.js
+++ displayerfunct.js
@@ -5,12 +5,12 @@ function displayOnScreen (carArrayParsed) {
     for (var i = 0; i < carArrayParsed.length;i++){
     var currentCar = carArrayParsed[i]
         carDisplay += `<div class="carCard">
-        <h1 class="modifiable">${currentCar.make}</h1>
-        <h2 class="modifiable">${currentCar.model}</h2>
-        <h3 class="modifiable">${currentCar.year}</h3>
-        <p  class="modifiable">${currentCar.price}</p>
-        <p  class="modifiable">${currentCar.color}</p>
-        <p  class="modifiable">${currentCar.purchased}<p>
+        <h1>${currentCar.make}</h1>
+        <h2>${currentCar.model}</h2>
+        <h3>${currentCar.year}</h3>
+        <p>${currentCar.price}</p>
+        <p>${currentCar.color}</p>
+        <p>${currentCar.purchased}<p>
         <p  class="modifiable">${currentCar.description}<p>
         </div>`
     }
--- eventhand.js
+++ eventhand.js
@@ -1,4 +1,3 @@
-
 var globalGo = function() {
     var listenArea = document.getElementsByClassName("modifiable");
         console.log(listenArea.length);
@@ -15,19 +14,27 @@ var globalGo = function() {
 function handleModifiableCLick() {
     console.log ("Element Clicked on", event.target);
     document.getElementById("inputField").focus();
-    // return {
-    //     elementClicked;
-    // }
     mirrorText(event.target);
 }
 
 
 function mirrorText (elementClicked) {
+    console.log(elementClicked);
     var inputField = document.getElementById("inputField");
     inputField.addEventListener("keyup", function (evt) {
         elementClicked.innerHTML = inputField.value;
     });
 }
+
+function doneButton () {
+    var button = document.getElementById("doneButton");
+    button.addEventListener("click", function () {
+        console.log("I've been clicked");
+
+
+    })
+}
+
 
 
 
